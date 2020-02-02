2447c7f332e406ac6a9c102d9b47d579f0e95852
Murat Baskıcıoğlu <murat.baskicioglu@getir.com>
2020-02-02 18:46:10 +0300

    Seperate main app creation logic to make it easy when deploy in further usages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { AbstractHttpAdapter, NestApplication, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { create } from 'domain';
+
+export async function createApp(httpAdapter?: AbstractHttpAdapter): Promise<INestApplication> {
+  let app: NestApplication;
+
+  if (httpAdapter) {
+    app = await NestFactory.create(AppModule, httpAdapter);
+  } else {
+    app = await NestFactory.create(AppModule);
+  }
+
+  return app;
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await createApp();
   await app.listen(5454);
 }
 bootstrap();
